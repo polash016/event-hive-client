36cce97633a6b6e4e3727c7ff1fe4eaa2eebe687
Md Robin Sarkar <robin.shrkr@gmail.com>
2024-08-28 00:44:53 +0600

    added date formatter

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,13 @@
+export const formatDate = (value: string) => {
+  const date = new Date(value);
+
+  const year = date.getFullYear();
+
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+
+  const day = date.getDate().toString().padStart(2, "0");
+
+  const formattedDate = `${year}-${month}-${day}`;
+
+  return formatDate;
+};
